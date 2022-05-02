464e9a54972d52ff71508d06b8c1fe59c62970ae
julia <j.kholstinina@yandex.ru>
2022-05-02 13:14:31 +0300

    animation with Transition and setPathMotion

--- app/src/main/java/com/example/movingcar/MainActivity.kt
+++ app/src/main/java/com/example/movingcar/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.movingcar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.ArcMotion
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
+import com.example.movingcar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private var toRightAnimation = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val customView = CustomView(this)
-        setContentView(customView)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        animateCarMovement()
+    }
+
+    private fun animateCarMovement() {
+        binding.carImageView.setOnClickListener {
+            val changeBounds = ChangeBounds()
+            changeBounds.setPathMotion(ArcMotion())
+            changeBounds.duration = ANIM_DURATION
+            TransitionManager.beginDelayedTransition(binding.container, changeBounds)
+            toRightAnimation = !toRightAnimation
+            val params = binding.carImageView.layoutParams as FrameLayout.LayoutParams
+            params.gravity = if (toRightAnimation) Gravity.END or Gravity.BOTTOM else Gravity.START or Gravity.TOP
+            binding.carImageView.layoutParams = params
+        }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val ANIM_DURATION = 10000L
     }
 }
